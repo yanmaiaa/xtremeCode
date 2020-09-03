00db1cfbd9b90f953ad4cb66495ac9139d31711e
Yan Maia <yandamasceno01@gmail.com>
2020-09-02 22:01:29 -0300

    Animações em JavaScript

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "intro",
+    time: 3500,
+  });
+  new SimpleSlide({
+    slide: "subtitulo",
+    time: 3500,
+    nav: true,
+  });
+}
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
