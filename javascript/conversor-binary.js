5553a4e1bf042849a8862edce5e91cff8633b90a
Yan Maia <yandamasceno01@gmail.com>
2020-09-05 12:22:32 -0300

    Ajustes - Html e Javascript

--- javascript/conversor-binary.js
+++ javascript/conversor-binary.js
@@ -1,8 +1,9 @@
 function converter() {
     const binario = document.getElementById('inp-1').value;
     if (binario === '') return alert('Por favor, insira um valor numérico');
-    binario.split('').map((char) => {
-        if (char !== '0' && char !== '1') return alert('Por favor, insira um valor válido');
+    binario.split('').map((caractere) => {
+        if (caractere !== '0' && caractere !== '1') 
+        return alert('Por favor, insira um valor válido');
     });
     const valor = parseInt(binario, 2);
     document.getElementById('inp-2').value = valor;
